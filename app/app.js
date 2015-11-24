63f480497871bb22f56a3cc7c8ab7113a565b5ab
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-11-24 04:57:07 +0100

    Use imports in app.js instead of script tags in the index

--- app/app.js
+++ app/app.js
@@ -1,4 +1,10 @@
 /// <reference path="../typings/angularjs/angular.d.ts" />
+import "klondike/game.js";
+import "klondike/board.js";
+import "klondike/klondike.js";
+import "klondike/scoring.js";
+
+
 
 angular.module("solitaire", ["klondike", "ngDraggable"]);
 angular.element(document).ready(() => {
